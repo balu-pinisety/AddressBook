dc5fac1254bdfb9a9783bf667f4b442a14c60f24
balu-pinisety <manikantapandu225@gmail.com>
2021-06-23 07:55:55 +0530

    Added: AddressBook.java with welcome meassage in it

---
+++ AddressBook.java
@@ -0,0 +1,13 @@
+package com.AddressBook;
+
+/**
+ * @author BALU
+ * Creating an Address Book of Persons details for user and can Add, Delete, Edit and Show it
+ */
+public class AddressBook {
+
+	public static void main(String[] args) {
+		// Displays welcome message
+		System.out.println("Welcome to Address Book Program");
+	}	
+}
