29bd441e5c8ed21c059e116c72e2caab1e31fbec
balu-pinisety <manikantapandu225@gmail.com>
2021-06-23 21:05:25 +0530

    Added: Method for to edit details of Contact by calling Name of Person

--- AddressBook.java
+++ AddressBook.java
@@ -11,21 +11,21 @@ public class AddressBook {
 
 	List<PersonDetails> personList = new ArrayList<>();
 	private static Scanner scan = new Scanner(System.in);
-	
+	public static AddressBook addressBook = new AddressBook();// Creating an object of Class AddressBook
 	public static void main(String[] args) {
 		// TODO display welcome message
 		System.out.println("Welcome to Address Book Program");
-		// Creating an object of Class AddressBook
-		AddressBook addressBook = new AddressBook();
 		// Calling method to add details of the person
 		boolean choice=true;
 		while (choice) {
-			System.out.println("\nChoose the option:\nPress '1' for Add\nPress '2' for Exit");
+			System.out.println("\nChoose the option:\nPress '1' for Add\nPress '2' for Edit\nPress '3' for Exit");
 			int option=scan.nextInt();
 			switch (option) {
 			case 1: addressBook.addDetails();
 				break;
-			case 2: choice=false;
+			case 2: addressBook.editDetails();
+				break;
+			case 3: choice=false;
 				System.out.println("Exited out of AddressBook");
 				break;
 			default: 
@@ -68,4 +68,72 @@ public class AddressBook {
 		System.out.println("\nGiven Details are added into the list"+person.getDetails());
 	}
 	
+	/** To display the names of persons that are added */
+	private void displayContacts() {
+		System.out.println("\nContacts in AddessBook:");
+		for (int i = 0; i < personList.size(); i++) {
+			String first=personList.get(i).getFirstName();
+			String last=personList.get(i).getLastName();
+			System.out.println((i+1)+") "+first+" "+last);
+		}
+	}
+		
+	/** Asks the user to edit existing details using the name of the person */
+	private void editDetails() {
+		addressBook.displayContacts();//To display contacts
+		System.out.print("Choose the number from above list that to be Edit: ");
+		int number = scan.nextInt();
+		PersonDetails details = personList.get(number-1);
+		System.out.println(details.getDetails());
+		System.out.println("\nEnter slot number that to be edit from above");
+		int slotnumber = scan.nextInt();
+		while(slotnumber<10) {
+			switch (slotnumber) {
+			case 0: slotnumber=10;
+				break;
+			case 1: System.out.print("\nEnter First name : ");
+				details.setFirstName(scan.next());
+				System.out.println("\nEnter other slot number to Edit or Press'0' to Exit");
+				slotnumber=scan.nextInt();
+				break;
+			case 2: System.out.print("\nEnter Last name : ");
+				details.setLastName(scan.next());
+				System.out.println("\nEnter other slot number to Edit or Press'0' to Exit");
+				slotnumber=scan.nextInt();
+				break;
+			case 3: System.out.print("Enter Address : ");
+				details.setAddress(scan.next());
+				System.out.println("\nEnter other slot number to Edit or Press'0' to Exit");
+				slotnumber=scan.nextInt();
+				break;
+			case 4: System.out.print("Enter City name : ");
+				details.setCity(scan.next());
+				System.out.println("\nEnter other slot number to Edit or Press'0' to Exit");
+				slotnumber=scan.nextInt();
+				break;
+			case 5: System.out.println("Enter State name : ");
+				details.setState(scan.next());
+				System.out.println("\nEnter other slot number to Edit or Press'0' to Exit");
+				slotnumber=scan.nextInt();
+				break;
+			case 6: System.out.println("Enter ZIP Code : ");
+				details.setZipCode(scan.nextInt());
+				System.out.println("\nEnter other slot number to Edit or Press'0' to Exit");
+				slotnumber=scan.nextInt();
+				break;
+			case 7: System.out.print("Enter Phone Number : ");
+				details.setPhoneNumber(scan.nextLong());
+				System.out.println("\nEnter other slot number to Edit or Press'0' to Exit");
+				slotnumber=scan.nextInt();
+				break;
+			case 8:	System.out.print("Enter E-Mail ID : ");
+				details.setEmail(scan.next());
+				System.out.println("\nEnter other slot number to Edit or Press'0' to Exit");
+				slotnumber=scan.nextInt();
+				break;
+			default: System.out.println("Enter valid input");
+			}
+		}
+		System.out.println("After Updating"+details.getDetails());
+	}
 }
